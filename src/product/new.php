1c29f32dd4bfb1ffe0f20d6e20cb8a37f42859cd
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-09 18:30:26 +0100

    ajout de l'attribut enctype pour le traitement des fichiers

--- src/product/new.php
+++ src/product/new.php
@@ -9,10 +9,11 @@ require_once __DIR__ . '/../../includes/header.php';
     <div class="container bg-light">
         <div class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
             <div class="py-5 bg-light">
-                <form method="post" action="">
+                <!-- use multipart/form-data when your form includes any <input type="file"> elements -->
+                <form method="post" enctype="multipart/form-data" action="">
                     <div class="form-group">
-                        <label for="nom">Nom</label>
-                        <input type="text" class="form-control" id="nom" placeholder="Nom du produit">
+                        <label for="name">Nom</label>
+                        <input type="text" class="form-control" id="name" placeholder="Nom du produit">
                     </div>
                     <div class="form-group">
                     <div class="form-group">
