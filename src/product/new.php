1a4a1ba6e58b65fb36e877ef245757a4735f211b
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-09 18:17:02 +0100

    creation de formulaire

--- src/product/new.php
+++ src/product/new.php
@@ -5,7 +5,26 @@ require_once __DIR__ . '/../../includes/header.php';
 
 ?>
 
-
+<main>
+    <div class="container bg-light">
+        <div class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
+            <div class="py-5 bg-light">
+                <form method="post" action="">
+                    <div class="form-group">
+                        <label for="nom">Nom</label>
+                        <input type="text" class="form-control" id="nom" placeholder="Nom du produit">
+                    </div>
+                    <div class="form-group">
+                    <div class="form-group">
+                        <label for="file">Fichier</label>
+                        <input type="file" class="form-control-file" id="file">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Valider</button>
+                </form> 
+            </div>
+        </div>
+    </div>
+</main>
 
 
 
