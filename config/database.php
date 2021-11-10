008dda2c14508cb256715b2b4a801d6fe3b294c5
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-10 11:02:20 +0100

    connexion à la bdd

--- config/database.php
+++ config/database.php
@@ -1,1 +1,12 @@
+<?php
 
+require_once __DIR__ . '/globals.php';
+
+$dbName = DB_NAME;
+$dbHost = DB_HOST;
+
+$dsn = "mysql:dbname=$dbName;host=$dbHost";
+
+$pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
+
+var_dump($pdo);
