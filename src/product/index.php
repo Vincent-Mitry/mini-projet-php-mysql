36ab1f9a5781595cf0b1c4980f518053d6c2f2fe
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-17 10:32:08 +0100

    mise à jour de l'index

--- src/product/index.php
+++ src/product/index.php
@@ -3,7 +3,7 @@
 require_once (__DIR__ . '/../../config/database.php');
 require_once (__DIR__ . '/../../includes/header.php');
 
-$sql = $pdo->query("SELECT `title`, `file`, `id` FROM `item`");
+$sql = $pdo->query("SELECT * FROM `item`");
 
 $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,15 +14,27 @@ $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
 <main>
     <div class="py-5 bg-light">
         <div class="container">
-            <div class="row">
-                <?php foreach($rows as $row): ?>
-                    <div class="col-md-4">
-                        <div class="card mb-4">
-                            <img src="<?= $row['file']; ?>" alt="" class="img-fluid">
-                            <a href="/divers/mini-projet-php-mysql/src/product/show.php?id=<?= $row['id'] ?>"><h3 class="card-title text-center"><?= $row['title'] ?></h3></a>
-                        </div>
-                    </div>
-                <?php endforeach ?>
+            <div class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i=1; foreach($rows as $row): ?>
+                        <tr>
+                            <th scope="row"><?=$i++?></th>
+                            <td><?= ($row['type'] == 1) ? "Film" : "Série" ?></td>
+                            <td><?= $row['title'] ?></td>
+                            <td><?= $row['description'] ?></td>
+                        </tr>
+                    <?php endforeach ?>    
+                    </tbody>
+                </table>
             </div>
             <a href="/divers/mini-projet-php-mysql/src/product/new.php" class="btn btn-primary">Ajout d'un Produit</a>
         </div>
